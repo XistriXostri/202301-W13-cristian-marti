fdf1aab92f42cad891e956700e56f258b2b5d059
xistri <cristian93marti@gmail.com>
2023-01-17 19:21:05 +0100

    add assets and css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './core/components/App';
+import App from './core/components/app/App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
